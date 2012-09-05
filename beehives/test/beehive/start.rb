b966078736a595f4956114289bd2fe8f1ed6966f
Michael Trommer <mictro@gmail.com>
2012-09-05 21:55:12 +0200

    modified start.rb and default start.rb

--- beehives/test/beehive/start.rb
+++ beehives/test/beehive/start.rb
@@ -1,21 +1,9 @@
-#
-#
-# Author:  Michael 'entropie' Trommer <mictro@gmail.com>
-#
-
 require "../../../lib/hive.rb"
 
 include Hive
 identifier = File.expand_path(__FILE__).split("/")[-3].to_sym
 
 Queen::hives.load( identifier )
-Queen::hives[ identifier ].start
-
-=begin
-Local Variables:
-  mode:ruby
-  fill-column:70
-  indent-tabs-mode:nil
-  ruby-indent-level:2
-End:
-=end
+beehive = Queen::hives[ identifier ]
+beehive.assets.read
+beehive.standalone!
