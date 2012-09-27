247bf8462197a9b903ac7161ec4ae458a8c9a3f4
Michael Trommer <mictro@gmail.com>
2012-09-27 19:02:20 +0200

    import task

--- Rakefile.rb
+++ Rakefile.rb
@@ -45,6 +45,32 @@ namespace :beehives do
   end
 end
 
+task :import_images do
+  Hives[:dir].require_enviroment!
+  Hives[:dir].set_enviroment
+
+  targets = %w(direct sidebar utils)
+
+  targets.each do |target|
+    tcdir = "/home/mit/Source/trailercamp/APPS/dogs/app/public/images/auto/#{target}"
+
+    irf = Helper::ImageResize
+    tfiles = Dir.glob(tcdir + "/orginal_*.*").map{ |f| File.readlink(f) }
+
+    target_dir = Hives[:dir].media_path("images", "categories", target)
+
+    tfiles.each do |tf|
+      fn = PluginMediaController.safe_file(tf, File.open(tf, 'rb'), File.join(target_dir, 'original'))
+
+      new_file = File.join(target_dir, 'original', fn)
+      Helper::ImageResize::ImageResizeFacility.new{
+        resize(new_file)
+      }.start(:thumbnail, :medium, :sidebar, :big)
+    end
+  end
+
+end
+
 =begin
 Local Variables:
   mode:ruby
