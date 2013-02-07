23d0b64cc57cbd2c2674c7040c04bf9b1f6af93a
Michael Trommer <mictro@gmail.com>
2013-02-07 11:54:58 +0100

    deploy tuned

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,15 +7,32 @@ set :scm, :git
 
 set :branch, "master"
 
-role :web, "pullies"
-role :app, "pullies"
-role :db,  "pullies", :primary => true
+
+BEEHIVES = Dir.glob("beehives/*").map { |b| File.basename(b) }
+
+requested_beehive = ARGV.first
+
+if requested_beehive and BEEHIVES.include?(requested_beehive)
+  case requested_beehive
+  when "klangwolke"
+    role :web,      "dynamiet.nine.ch"
+    role :app,      "dynamiet.nine.ch"
+    role :db,       "dynamiet.nine.ch", :primary => true
+  else
+    role :web,      "pullies"
+    role :app,      "pullies"
+    role :db,       "pullies",          :primary => true
+  end
+end
 
 set :deploy_via,                  :remote_cache
 set :normalize_asset_timestamps,  false
 set :git_enable_submodules,       false
 
-Dir.glob("beehives/*").each do |beehive|
+
+
+
+BEEHIVES.each do |beehive|
   beehive = File.basename(beehive).to_sym
 
   next if beehive == :test
@@ -58,6 +75,35 @@ Dir.glob("beehives/*").each do |beehive|
         dirs = [deploy_to, releases_path, shared_path]
         run "mkdir -p #{dirs.join(' ')}"
         run "chmod g+w #{dirs.join(' ')}" if fetch(:group_writable, true)
+      end
+    end
+
+
+    if beehive == :klangwolke
+      #
+      # UPLOADS
+      #
+      namespace :uploads do
+        desc "Creates the upload folders unless they exist and sets the proper upload permissions."
+        task :setup, :except => { :no_release => true } do
+          dirs = uploads_dirs.map { |d| File.join(shared_path, d) }
+          run "mkdir -p #{dirs.join(' ')} && chmod g+w #{dirs.join(' ')}"
+        end
+
+        desc "[internal] Creates the symlink to uploads shared folder for the most recently deployed version."
+        task :symlink, :except => { :no_release => true } do
+          run "rm -rf #{release_path}/beehives/klangwolke/media"
+          run "ln -nfs #{shared_path}/media #{release_path}/beehives/#{beehive}/media"
+        end
+
+        desc "[internal] Computes uploads directory paths and registers them in Capistrano environment."
+        task :register_dirs do
+          set :uploads_dirs,    %w(media)
+          set :shared_children, fetch(:shared_children) + fetch(:uploads_dirs)
+        end
+
+        after       "deploy:update_beehive", "uploads:symlink"
+        on :start,  "uploads:register_dirs"
       end
     end
 
